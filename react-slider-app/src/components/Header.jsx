c4cad72edc34de5d02514188f6548e254824a77e
Luis Sanchez <luisanchezdh@gmail.com>
2022-10-28 11:27:27 -0400

    finished

--- react-slider-app/src/components/Header.jsx
+++ react-slider-app/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import logo from "../img/valorant-logo.png"
+import background from "../img/white.webp"
 
 const Header = () => {
   return (
     <header className="center">
-      <img className ="logo"src={logo} alt="" />
+      <div className="parent">
+      <img className="logo" src={logo} alt="" />
+        <img className="backgroundImg" src={background} alt="" />
+        </div>
     </header>
   )
 }
